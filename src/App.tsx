fa4ef4d317191bf75d6150b06308f8c78ba9f901
Victoria Nogueira Marques <victoriamarques0604@gmail.com>
2024-04-20 15:50:26 -0300

    Update on bg image

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import HomeMobile from "./pages/mobile/home";
 
 export default function App() {
   return (
-    <div className="flex flex-col bg-darkblue w-screen h-screen">
+    <div className="flex flex-col w-screen h-screen text-white bg-[url('./src/components/background-stars.svg')] bg-no-repeat bg-center bg-darkblue ">
       <HomeMobile />
     </div>
   );
