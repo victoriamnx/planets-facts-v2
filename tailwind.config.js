ba6abe22bdaa4fc354825dfc6ad3f4024ae08a7c
Victoria Nogueira Marques <victoriamarques0604@gmail.com>
2024-04-20 12:02:34 -0300

    add: new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      white: "#ffffff",
+      darkblue: "#070724",
+      darkgrey: "#38384F",
+      grey: "#838391",
+      purple: "#6f2ed6",
+      blue: "#2d68f0",
+      smerald: "#419EBB",
+      marine: "#1ec2a4",
+      red: "#D83A34",
+      orange: "#D14C32",
+      softorange: "#CD5120",
+      yellow: "#EDA249",
+      whiteOp: "#FFFFFF1A",
+    },
+    extend: {
+      fontFamily: {
+        antonio: ["Antonio", "sans-serif"],
+        spartan: ["League Spartan", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
